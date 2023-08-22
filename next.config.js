981bc34f2211d7ab42b89db079bb5a6450bd44f2
kanghunK <ltbehappy16@gmail.com>
2023-08-22 15:25:20 +0900

    Add: proxy 설정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async rewrites() {
+        if (process.env.NODE_ENV === "development") {
+            return [
+                {
+                    source: "/",
+                    destination: "https://codakcodak.site",
+                },
+            ];
+        }
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
