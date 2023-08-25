70cb02776c63d75f1efc49778cef35a3a7e8eed0
kanghunK <ltbehappy16@gmail.com>
2023-08-25 15:42:05 +0900

    Modify: userData.ts로 네이밍, useAuth 에러 처리 Add: useRoom 훅 추가

--- states/stores/userData.ts
+++ states/stores/userData.ts
@@ -2,69 +2,66 @@ import { AxiosError } from "axios";
 import useStore from "swr-global-state";
 import axios from "@/lib/api";
 import localStoragePersistor from "../persistors/local-storage";
+import { DRoomData, RoomData } from "@/lib/types";
 
-const USER_KEY = "@user/auth";
+const AUTH_KEY = "@user/auth";
 const TOKEN_KEY = "@user/token";
+const ROOM_KEY = "@user/room";
 
 export function useAuth() {
     const [isLoading, setLoading] = useStore({
-        key: `${USER_KEY}-loading`,
+        key: `${AUTH_KEY}-loading`,
         initial: true,
     });
-    const [data, , swrDefaultResponse] = useStore(
-        {
-            key: USER_KEY,
-            initial: null,
-            persistor: {
-                onSet: localStoragePersistor.onSet,
-                onGet: async (key) => {
-                    try {
-                        const tokenData =
-                            localStoragePersistor.onGet(TOKEN_KEY);
-                        if (tokenData) {
-                            const token = tokenData.access_token;
-                            // 유저 정보 확인요청
-                            const response = await axios.get("/user/my", {
-                                headers: {
-                                    Authorization: token,
-                                },
-                            });
-                            const userData = {
-                                isLoggedIn: true,
-                                user_info: response.data.user_info,
-                            };
-                            localStoragePersistor.onSet(key, userData);
-                            return userData;
-                        }
-                        const errorObj = new Error(
-                            "사용자 정보가 없어 로그아웃됩니다."
-                        );
-                        errorObj.name = "NoToken";
-                        throw errorObj;
-                    } catch (err: unknown) {
-                        if (window.navigator.onLine) {
-                            if (err instanceof AxiosError) {
-                                const errorObj = new Error(
-                                    "사용자 정보가 일치하지 않아 로그아웃됩니다."
-                                );
-                                errorObj.name = "InvalidUserData";
-                                throw errorObj;
-                            } else {
-                                throw err;
-                            }
-                        }
-                        const cachedData = localStoragePersistor.onGet(key);
-                        return cachedData;
-                    } finally {
-                        setLoading(false);
+    const [data, , swrDefaultResponse] = useStore({
+        key: AUTH_KEY,
+        initial: null,
+        persistor: {
+            onSet: localStoragePersistor.onSet,
+            onGet: async (key) => {
+                try {
+                    const tokenData = localStoragePersistor.onGet(TOKEN_KEY);
+
+                    if (!tokenData) {
+                        console.log("get 확인");
+                        return {
+                            isLoggedIn: false,
+                            user_info: null,
+                        };
                     }
-                },
+
+                    // 유저 정보 확인요청
+                    const response = await axios.get("/user/my", {
+                        headers: {
+                            Authorization: tokenData.access_token,
+                        },
+                    });
+                    const userData = {
+                        isLoggedIn: true,
+                        user_info: response.data.user_info,
+                    };
+                    localStoragePersistor.onSet(key, userData);
+
+                    return userData;
+                } catch (err: unknown) {
+                    console.error(err);
+                    if (window.navigator.onLine) {
+                        if (err instanceof AxiosError) {
+                            const errorObj = new Error(
+                                "사용자 정보가 일치하지 않아 로그아웃됩니다."
+                            );
+                            errorObj.name = "InvalidUserData";
+                            throw errorObj;
+                        }
+                    }
+                    const cachedData = localStoragePersistor.onGet(key);
+                    return cachedData;
+                } finally {
+                    setLoading(false);
+                }
             },
         },
-        {
-            revalidateOnReconnect: true,
-        }
-    );
+    });
 
     const { mutate: loginMutate, error: loginError } = swrDefaultResponse;
 
@@ -98,9 +95,14 @@ export function useAuth() {
 
     const logout = async () => {
         setLoading(true);
-        window.localStorage.removeItem(USER_KEY);
+        window.localStorage.removeItem(AUTH_KEY);
         window.localStorage.removeItem(TOKEN_KEY);
-        loginMutate(null);
+        window.localStorage.removeItem(ROOM_KEY);
+
+        loginMutate({
+            isLoggedIn: false,
+            user_info: null,
+        });
         setLoading(false);
     };
 
@@ -110,5 +112,87 @@ export function useAuth() {
         isLoading,
         login,
         logout,
+    };
+}
+
+export function useRoom() {
+    const [isLoading, setLoading] = useStore({
+        key: `${ROOM_KEY}-loading`,
+        initial: true,
+    });
+    const [data, , swrDefaultResponse] = useStore<RoomData[] | null, any>({
+        key: ROOM_KEY,
+        initial: null,
+        persistor: {
+            onSet: localStoragePersistor.onSet,
+            onGet: async (key) => {
+                try {
+                    const tokenData = localStoragePersistor.onGet(TOKEN_KEY);
+                    const userInfoData = localStoragePersistor.onGet(AUTH_KEY);
+
+                    if (!tokenData || !userInfoData) throw new Error();
+
+                    const response = await axios.get(
+                        `/user/${userInfoData.user_info.id}/roomlist`,
+                        {
+                            headers: {
+                                Authorization: tokenData.access_token,
+                            },
+                        }
+                    );
+
+                    const roomlist: RoomData[] = response.data.roomlist.map(
+                        (roomData: DRoomData) => {
+                            const userlist = roomData.userlist.map((data) => ({
+                                id: data.id,
+                                name: data.name,
+                            }));
+                            return { ...roomData, userlist };
+                        }
+                    );
+
+                    localStoragePersistor.onSet(key, roomlist);
+
+                    return roomlist;
+                } catch (error: unknown) {
+                    if (window.navigator.onLine) {
+                        if (error instanceof AxiosError) {
+                            if (error.status === 401) {
+                                const errorObj = new Error(
+                                    "사용자 정보가 없어 로그아웃됩니다."
+                                );
+                                errorObj.name = "InvalidUserData";
+                                throw errorObj;
+                            } else if (error.status === 403) {
+                                const errorObj = new Error(
+                                    "권한을 가진 이용자가 아닙니다.."
+                                );
+                                errorObj.name = "IncorrectAuth";
+                                throw errorObj;
+                            }
+                        } else {
+                            console.error("에러", error);
+                            const errorObj = new Error(
+                                "사용자 정보가 없습니다! 다시 로그인 해주세요.."
+                            );
+                            errorObj.name = "NoLocalStorageInfo";
+                            throw errorObj;
+                        }
+                    }
+                    const cachedData = localStoragePersistor.onGet(key);
+                    return cachedData;
+                } finally {
+                    setLoading(false);
+                }
+            },
+        },
+    });
+
+    const { error } = swrDefaultResponse;
+
+    return {
+        data,
+        isLoading,
+        error,
     };
 }
