069f4cf88562ade6c00d07f38600b52960dd8074
kanghunK <ltbehappy16@gmail.com>
2023-08-29 16:29:20 +0900

    Add:  useFriend Hook 작성

--- states/stores/userData.ts
+++ states/stores/userData.ts
@@ -2,11 +2,12 @@ import { AxiosError } from "axios";
 import useStore from "swr-global-state";
 import axios from "@/lib/api";
 import localStoragePersistor from "../persistors/local-storage";
-import { DRoomData, RoomData } from "@/lib/types";
+import { DFriendData, DRoomData, RoomData } from "@/lib/types";
 
 const AUTH_KEY = "@user/auth";
 const TOKEN_KEY = "@user/token";
 const ROOM_KEY = "@user/room";
+const FRIEND_KEY = "@user/friend";
 
 export function useAuth() {
     const [isLoading, setLoading] = useStore({
@@ -23,7 +24,6 @@ export function useAuth() {
                     const tokenData = localStoragePersistor.onGet(TOKEN_KEY);
 
                     if (!tokenData) {
-                        console.log("get 확인");
                         return {
                             isLoggedIn: false,
                             user_info: null,
@@ -36,10 +36,12 @@ export function useAuth() {
                             Authorization: tokenData.access_token,
                         },
                     });
+
                     const userData = {
                         isLoggedIn: true,
                         user_info: response.data.user_info,
                     };
+
                     localStoragePersistor.onSet(key, userData);
 
                     return userData;
@@ -47,11 +49,13 @@ export function useAuth() {
                     console.error(err);
                     if (window.navigator.onLine) {
                         if (err instanceof AxiosError) {
-                            const errorObj = new Error(
-                                "사용자 정보가 일치하지 않아 로그아웃됩니다."
-                            );
-                            errorObj.name = "InvalidUserData";
-                            throw errorObj;
+                            if (err.response?.status === 401) {
+                                const errorObj = new Error(
+                                    "올바른 접속이 아님으로 로그아웃됩니다.."
+                                );
+                                errorObj.name = "InvalidUserData";
+                                throw errorObj;
+                            }
                         }
                     }
                     const cachedData = localStoragePersistor.onGet(key);
@@ -80,10 +84,12 @@ export function useAuth() {
                 password,
             });
 
-            window.localStorage.setItem(
-                TOKEN_KEY,
-                JSON.stringify(authData.data)
-            );
+            // window.localStorage.setItem(
+            //     TOKEN_KEY,
+            //     JSON.stringify(authData.data)
+            // );
+            localStoragePersistor.onSet(TOKEN_KEY, authData.data);
+
             loginMutate();
             setLoading(false);
         } catch (error: unknown) {
@@ -151,8 +157,6 @@ export function useRoom() {
                         }
                     );
 
-                    localStoragePersistor.onSet(key, roomlist);
-
                     return roomlist;
                 } catch (error: unknown) {
                     if (window.navigator.onLine) {
@@ -179,8 +183,110 @@ export function useRoom() {
                             throw errorObj;
                         }
                     }
-                    const cachedData = localStoragePersistor.onGet(key);
-                    return cachedData;
+                    const errorObj = new Error(
+                        "네트워크에 연결되어있지 않습니다.."
+                    );
+                    errorObj.name = "NetworkError";
+                    throw errorObj;
+                } finally {
+                    setLoading(false);
+                }
+            },
+        },
+    });
+
+    const { mutate: roomListMutate, error } = swrDefaultResponse;
+
+    const createRoom = async ({
+        userlist,
+        title,
+    }: {
+        userlist: number[];
+        title: string;
+    }) => {
+        setLoading(true);
+
+        const tokenData = localStoragePersistor.onGet(TOKEN_KEY);
+
+        await axios.post(
+            "/room",
+            { userlist, title },
+            {
+                headers: {
+                    Authorization: tokenData.access_token,
+                },
+            }
+        );
+
+        roomListMutate();
+        setLoading(false);
+    };
+
+    return {
+        data,
+        isLoading,
+        error,
+        createRoom,
+    };
+}
+
+export function useFriend() {
+    const [isLoading, setLoading] = useStore({
+        key: `${FRIEND_KEY}-loading`,
+        initial: true,
+    });
+    const [data, , swrDefaultResponse] = useStore<DFriendData[] | null, any>({
+        key: FRIEND_KEY,
+        initial: null,
+        persistor: {
+            onSet: localStoragePersistor.onSet,
+            onGet: async (key) => {
+                try {
+                    const tokenData = localStoragePersistor.onGet(TOKEN_KEY);
+                    const userInfoData = localStoragePersistor.onGet(AUTH_KEY);
+
+                    if (!tokenData || !userInfoData) throw new Error();
+
+                    const response = await axios.get(
+                        `/user/${userInfoData.user_info.id}/friendlist`,
+                        {
+                            headers: {
+                                Authorization: tokenData.access_token,
+                            },
+                        }
+                    );
+
+                    return response.data.friendlist;
+                } catch (err: unknown) {
+                    if (window.navigator.onLine) {
+                        if (error instanceof AxiosError) {
+                            if (error.status === 401) {
+                                const errorObj = new Error(
+                                    "사용자 정보가 없어 로그아웃됩니다."
+                                );
+                                errorObj.name = "InvalidUserData";
+                                throw errorObj;
+                            } else if (error.status === 403) {
+                                const errorObj = new Error(
+                                    "권한을 가진 이용자가 아닙니다.."
+                                );
+                                errorObj.name = "IncorrectAuth";
+                                throw errorObj;
+                            }
+                        } else {
+                            console.error("에러", error);
+                            const errorObj = new Error(
+                                "사용자 정보가 없습니다! 다시 로그인 해주세요.."
+                            );
+                            errorObj.name = "NoLocalStorageInfo";
+                            throw errorObj;
+                        }
+                    }
+                    const errorObj = new Error(
+                        "네트워크에 연결되어있지 않습니다.."
+                    );
+                    errorObj.name = "NetworkError";
+                    throw errorObj;
                 } finally {
                     setLoading(false);
                 }
