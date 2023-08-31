e7ad956d217e49d460322b6d52b807757e76921d
kanghunK <ltbehappy16@gmail.com>
2023-08-31 17:25:13 +0900

    Add: room 페이지 subTitle

--- app/(private_route)/room/page.tsx
+++ app/(private_route)/room/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useCallback } from "react";
 import { useRoom } from "@/states/stores/userData";
 import Card from "@/components/Card";
 import { RoomData } from "@/lib/types";
@@ -8,39 +8,19 @@ import styles from "./room.module.scss";
 
 export default function Room() {
     const { data: roomlist, isLoading } = useRoom();
-    // console.log("확인", roomlist, error);
 
     if (isLoading || !roomlist) return <div>로딩중...</div>;
 
-    const dummyData: RoomData[] = [
-        {
-            id: 1,
-            title: "room1",
-            host_user_id: 1,
-            userlist: [
-                { id: 1, name: "test" },
-                { id: 2, name: "test2" },
-                { id: 3, name: "test3" },
-            ],
-        },
-        {
-            id: 2,
-            title: "room2",
-            host_user_id: 2,
-            userlist: [
-                { id: 2, name: "test2" },
-                { id: 5, name: "test5" },
-                { id: 3, name: "test3" },
-                { id: 4, name: "test4" },
-            ],
-        },
-    ];
-
     return (
-        <div className={styles.card_wrapper}>
-            {dummyData.map((roomData) => (
-                <Card key={roomData.id} roomData={roomData} />
-            ))}
-        </div>
+        <>
+            <div className={styles.subtitle}>
+                <h2>방 목록</h2>
+            </div>
+            <div className={styles.card_wrapper}>
+                {roomlist.map((roomData) => (
+                    <Card key={roomData.id} roomData={roomData} />
+                ))}
+            </div>
+        </>
     );
 }
