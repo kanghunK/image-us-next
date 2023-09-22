c03c00e94e928b56667424993e477cb0f510f2ae
kanghunK <ltbehappy16@gmail.com>
2023-09-22 21:09:51 +0900

    Add: favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,18 @@
 import { RootLayoutChildren } from "@/lib/types";
 import "./globals.css";
 
+export const metadata = {
+    icons: {
+        icon: "/favicon.ico",
+    },
+};
+
 export default function RootLayout({ children }: RootLayoutChildren) {
     return (
         <html lang="en">
+            <head>
+                <link rel="shortcut icon" href="/favicon.ico" />
+            </head>
             <body>{children}</body>
         </html>
     );
