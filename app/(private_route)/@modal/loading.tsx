e10db184b16888431cfd4466e6e3c2ae62f8a014
kanghunK <ltbehappy16@gmail.com>
2023-09-23 12:41:52 +0900

    Modal loading skeleton 추가

--- app/(private_route)/@modal/loading.tsx
+++ app/(private_route)/@modal/loading.tsx
@@ -1,3 +1,14 @@
+import Modal from "@/components/shared/Modal";
+import styles from "./styles.module.css";
+
 export default function Loading() {
-    return null;
+    return (
+        <Modal>
+            <div className={styles.loading_skeleton_container}>
+                <div className={styles.react_loading_skeleton}></div>
+                <div className={styles.react_loading_skeleton}></div>
+                <div className={styles.react_loading_skeleton}></div>
+            </div>
+        </Modal>
+    );
 }
