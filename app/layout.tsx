87c01c5144d3b37bb5c0df692122a55fcc2b4ed0
kanghunK <ltbehappy16@gmail.com>
2023-10-03 17:11:54 +0900

    Delete: metadata 변수

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,6 @@
 import { RootLayoutChildren } from "@/lib/types";
 import "./globals.css";
 
-export const metadata = {
-    icons: {
-        icon: "/favicon.ico",
-    },
-};
-
 export default function RootLayout({ children }: RootLayoutChildren) {
     return (
         <html lang="en">
