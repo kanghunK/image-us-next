05f582a7052d33dc2e7d2b2d6d3934cf89e2c720
kanghunK <ltbehappy16@gmail.com>
2023-10-04 15:04:43 +0900

    rewrites 설정 제거

--- next.config.js
+++ next.config.js
@@ -1,17 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-        if (process.env.NODE_ENV === "development") {
-            return [
-                {
-                    source: "/backapi/:path*",
-                    destination: "https://codakcodak.site/backapi/:path*",
-                },
-            ];
-        }
-        return [];
-    },
-    // output: "export",
     distDir: "dist",
     skipTrailingSlashRedirect: true,
     compiler: {
