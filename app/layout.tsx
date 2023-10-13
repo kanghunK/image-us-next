f055e7de14f7590f483f8b74342bc5d0cd1b8e9c
kanghunK <ltbehappy16@gmail.com>
2023-10-13 15:31:50 +0900

    Add: head의 title 태그 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ export default function RootLayout({ children }: RootLayoutChildren) {
         <html lang="en">
             <head>
                 <link rel="shortcut icon" href="/favicon.ico" />
+                <title>ImageUs</title>
             </head>
             <body>{children}</body>
         </html>
