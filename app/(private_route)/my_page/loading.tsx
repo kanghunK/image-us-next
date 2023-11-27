4f6e700d89fa7b98f3dcb21eea0b122d69322114
kanghunK <ltbehappy16@gmail.com>
2023-11-27 23:13:52 +0900

    마이페이지 로딩 컴포넌트 수정

--- app/(private_route)/my_page/loading.tsx
+++ app/(private_route)/my_page/loading.tsx
@@ -1,8 +1,29 @@
 "use client";
 
 import React from "react";
+import styled from "@emotion/styled";
+import { roboto } from "@/app/fonts";
 import LoadingCompoent from "@/components/shared/Loading";
 
 export default function Loading() {
-    return <LoadingCompoent />;
+    return (
+        <LoadingContainer>
+            <div className={`${roboto.className} loading_text`}>
+                <span className="text">정보 불러오는중</span>
+            </div>
+            <LoadingCompoent loadingType="balls" />
+        </LoadingContainer>
+    );
 }
+
+const LoadingContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+
+    .text {
+        font-size: 1.2rem;
+    }
+`;
